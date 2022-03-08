fe3d437434fb1743a5ba96afdf309383daeb96fb
bevaris <mat.evaristo@gmail.com>
2022-03-08 20:04:59 -0300

    exercicio-apis-labenusers

--- modulo3/apis-labenusers/apis-labenusers/src/App.js
+++ modulo3/apis-labenusers/apis-labenusers/src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Teste</h1>
-    </div>
-  );
+class App extends React.Component {
+  pegarPlaylists = () => {
+    axios
+      .get(
+        'https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists',
+      {
+        headers: {
+          Authorization: 'matheus-bevar-guimaraes'
+        }
+      }
+    )
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+      <h1>oi silvio</h1>
+      <button onClick={this.pegarPlaylists}>PEGAR PLAYLISTS</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
